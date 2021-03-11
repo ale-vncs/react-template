9e8c9a5fc5214c9c9586d20bdbb2001b4af9458e
Alessandro Santos <alessandro.vncs@gmail.com>
2021-03-11 13:07:53 -0300

    -- Ajustes;

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import Button from '@components/Button';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import Button from '@components/Button'
+import ReactDOM from 'react-dom'
 
 const App = () => (
   <>
     <Button name={'Aperte'} />
     <h1>My React and TypeScript App!</h1>
   </>
-);
+)
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
-);
+  document.getElementById('root')
+)
